2a59746bf253813eafb776cde4ca5df605d86d47
John Penny <jpenny1993@users.noreply.github.com>
2020-01-19 15:43:37 +0000

    Implemented insertion sort

--- src/Algorithms.Demo/Program.cs
+++ src/Algorithms.Demo/Program.cs
@@ -13,28 +13,82 @@ namespace Algorithms.Demo
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Sort Times!\r\n");
 
             _dictionary = new Dictionary<string, List<TimeSpan>>();
-
             var random = new Random();
-            var numbers = (new int[10000]).Select(x => random.Next(0, 10000)).ToArray();
 
-            RecordSortTime("Linq", new LinqSorter<int>(), numbers);
+            int minValue = ReadIntAbs("Min value", 0);
+            int maxValue = ReadIntAbs("Max value", 10000, minValue);
+            int testSize = ReadIntAbs("Array size", 10000, 10);
+            int iterations = ReadIntAbs("Iterations per test", 50, 1);
+            Console.WriteLine();
 
-            RecordSortTime("QuickSort", new QuickSorter<int>(), numbers);
+            // Create a random test sample
+            var numbers = (new int[testSize]).Select(x => random.Next(minValue, maxValue)).ToArray();
 
-            RecordSortTime("BubbleSort", new BubbleSorter<int>(), numbers);
+            if (AskAction("Linq OrderBy"))
+                RecordSortTime("Linq", new LinqSorter<int>(), iterations, numbers);
+
+            if (AskAction("QuickSort"))
+                RecordSortTime("QuickSort", new QuickSorter<int>(), iterations, numbers);
+
+            if (AskAction("InsertionSort"))
+                RecordSortTime("InsertionSort", new InsertionSorter<int>(), iterations, numbers);
+
+            if (AskAction("BubbleSort"))
+                RecordSortTime("BubbleSort", new BubbleSorter<int>(), iterations, numbers);
 
             WriteResults();
+
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
         }
 
-        private static void RecordSortTime(string key, ISorter<int> sorter, IEnumerable<int> enumerable)
+        private static int ReadIntAbs(string message, int defaultValue, int minValue = 0) 
+        {
+            Console.Write("{0} [{1}]> ", message, defaultValue);
+            var input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input) &&
+                int.TryParse(input, out var number)) 
+            {
+                number = Math.Abs(number);
+                return number < minValue ? minValue : number;
+            }
+
+            return defaultValue;
+        }
+
+        private static bool AskAction(string message) 
+        {
+            do
+            {
+                Console.Write("{0} [Y/N]> ", message);
+                var input = Console.ReadKey();
+                Console.WriteLine("\r\n");
+
+                if (input.Key == ConsoleKey.Y) return true;
+                if (input.Key == ConsoleKey.N) return false;
+            } while (true);
+        }
+
+        private static string GetTime(TimeSpan time)
+        {
+            if (time.Seconds > 0)
+            {
+                return $"{time.TotalSeconds: 0.000} secs";
+            }
+
+            return $"{time.TotalMilliseconds} ms";
+        }
+
+        private static void RecordSortTime(string key, ISorter<int> sorter, int iterations, IEnumerable<int> enumerable)
         {
             var hasKey = _dictionary.ContainsKey(key);
             var results = hasKey ? _dictionary[key] : new List<TimeSpan>();
 
-            for (var i = 1; i < 51; i++) 
+            for (var i = 1; i < iterations + 1; i++) 
             {
                 Console.WriteLine("Begin Test: {0} v{1}", key, i);
                 IList<int> array = enumerable.Select(x => x).ToArray();
@@ -80,6 +134,11 @@ namespace Algorithms.Demo
 
         private static void WriteResults()
         {
+            if (!_dictionary.Keys.Any())
+                return;
+
+            Console.WriteLine(" ------------ ");
+            Console.WriteLine("    Results   ");
             Console.WriteLine(" ------------ \r\n");
             foreach (var key in _dictionary.Keys) 
             {
@@ -87,7 +146,7 @@ namespace Algorithms.Demo
 
                 if (!results.Any()) 
                 {
-                    Console.WriteLine("{0} - Successful: 0\r\n", key);
+                    Console.WriteLine(" {0, -13} \t Sort was not succesful.\r\n", key);
                     continue;
                 }
 
@@ -99,27 +158,16 @@ namespace Algorithms.Demo
                 // Attempts faster than the average
                 var aboveAvergageCount = results.Where(x => x < averageTime).Count();
 
-                var template = "{0} - Successful: {1}, > Avg: {2}, Avg: {3}, Fastest: {4}, Slowest: {5}\r\n";
+                var template = " {0, -13} \t Avg: {1}, Fastest: {2}, Slowest: {3}, {4} attempts faster than avg.\r\n";
                 Console.WriteLine(template, key,
-                    results.Count,
-                    aboveAvergageCount,
                     GetTime(averageTime),
                     GetTime(fastestTime), 
-                    GetTime(slowestTime)
+                    GetTime(slowestTime),
+                    aboveAvergageCount
                 );
 
             }
-            Console.WriteLine(" ------------ ");
-        }
-
-        private static string GetTime(TimeSpan time) 
-        {
-            if (time.Seconds > 0)
-            {
-                return $"{time.TotalSeconds : 0.000} secs";
-            }
-
-            return $"{time.TotalMilliseconds} ms";
+            Console.WriteLine(" ------------ \r\n");
         }
     }
 }
---
+++ src/Algorithms/Sort/InsertionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Sort
+{
+    public class InsertionSorter<T> : ISorter<T> where T : struct, IComparable<T>
+    {
+        public IList<T> Sort(IEnumerable<T> source)
+        {
+            IList<T> collection = source.ToArray();
+            Sort(ref collection);
+            return collection;
+        }
+
+        public void Sort(ref IList<T> source) => InsertionSort(ref source);
+
+        public void InsertionSort(ref IList<T> source) 
+        {
+            T element;
+            int j;
+
+            // Iterate though every element in the list
+            for (var x = 1; x < source.Count; x++)
+            {
+                element = source[x];
+                j = x - 1;
+
+                // Move all larger elements past the current element
+                while (j >= 0 && source[j].CompareTo(element) > 0)
+                {
+                    source[j + 1] = source[j];
+                    j--;
+                }
+
+                // Insert the current element
+                source[j + 1] = element;
+            }
+        }
+    }
+}
