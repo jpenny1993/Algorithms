4a3bc25e67cc26d3ddab0d6eb7701e260236b555
John Penny <jpenny1993@users.noreply.github.com>
2020-01-25 23:31:11 +0000

    Implemented Ternary Search

---
+++ src/Algorithms/Search/TernarySearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Search
+{
+    public class TernarySearcher<T> : ISearcher<T> where T : IComparable<T>
+    {
+        public int IndexOf(IList<T> source, T item)
+        {
+            int left = 0, right = source.Count - 1;
+            do
+            {
+                // Split the array into 3 regions and get the midpoints
+                int leftMid = left + (right - left) / 3;
+                int rightMid = right - (right - left) / 3;
+
+                // Check check for the item
+                var leftComparison = source[leftMid].CompareTo(item);
+                if (leftComparison == 0)
+                {
+                    return leftMid;
+                }
+
+                var rightComparison = source[rightMid].CompareTo(item);
+                if (rightComparison == 0)
+                {
+                    return rightMid;
+                }
+
+                // Reduce to the region containing the item
+                if (leftComparison > 0)
+                {
+                    // The item is between left and leftMid
+                    right = leftMid - 1;
+                }
+                else if (rightComparison < 0)
+                {
+                    // The item is between rightMid and right
+                    left = rightMid + 1;
+                }
+                else
+                {
+                    // The item is between leftMid and rightMid 
+                    left = leftMid + 1;
+                    right = rightMid - 1;
+                }
+            } while (right >= left);
+
+            // Not found
+            return -1;
+        }
+    }
+}
